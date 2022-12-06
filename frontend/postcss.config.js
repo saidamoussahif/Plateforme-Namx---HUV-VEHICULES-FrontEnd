40e94fdf38f9ff7c55c9ee950db47ead9674bd27
ayoub benouahi <abenouahi5@gmail.com>
2022-12-06 11:36:20 +0100

    add

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,14 +2,9 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-<<<<<<< HEAD
     plugins: [
       require("tailwindcss"),
       require("autoprefixer"),
     ]
   }
 };
-=======
-  },
-}
->>>>>>> 2d6e381ebaf6bffb1a908341c9b4a8c700540501
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
  content: [
-=======
-  content: [
->>>>>>> 2d6e381ebaf6bffb1a908341c9b4a8c700540501
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2d6e381ebaf6bffb1a908341c9b4a8c700540501
