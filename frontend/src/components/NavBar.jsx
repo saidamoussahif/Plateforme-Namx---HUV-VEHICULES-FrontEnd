a92fad238ad0bf2e6cadc56376464b1cd7bb7187
saidamoussahif <moussahifsaida@.gmail.com>
2022-12-15 09:59:48 +0100

    delete car

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,9 +12,9 @@ function NavBar() {
               className="w-24 h-24 flex justify-start "
                 alt="logo"
             />
-          </a>
+          </a> 
           <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900">First Link</a>
+            <a class="mr-5 hover:text-gray-900">Home</a>
             <a class="mr-5 hover:text-gray-900">Second Link</a>
             <a class="mr-5 hover:text-gray-900">Third Link</a>
             <a class="mr-5 hover:text-gray-900">Fourth Link</a>
